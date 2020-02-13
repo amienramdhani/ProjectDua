89c9b547d309e9a40f3f9115f1ba33fc202531fd
Ahmad Dulloh <59985636+madzdul17@users.noreply.github.com>
2020-02-14 02:49:46 +0700

    nambah login

--- app/src/main/java/com/example/projectdua/login.java
+++ app/src/main/java/com/example/projectdua/login.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class login extends AppCompatActivity {
 
 
 
@@ -13,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
